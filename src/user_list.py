744db8f06dffc18f1b0ce946af8a9ceed72e98ad
Thomas Lee <lisihan969@gmail.com>
2015-08-10 09:52:09 +0800

    Correct user_list.py

--- src/user_list.py
+++ src/user_list.py
@@ -1,5 +1,6 @@
+from PyQt5.QtWidgets import QListWidget
+
 class UserList(QListWidget):
     """User list"""
     def __init__(self, parent=None):
         super(UserList, self).__init__(parent)
-        self.setupUi(self)
