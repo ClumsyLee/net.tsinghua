131d1103d675a0ae5b35a7021311d2e17e471ee9
Thomas Lee <lisihan969@gmail.com>
2015-10-10 18:19:03 +0800

    Make Gruntfile more readable

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,48 +6,39 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    // Custom configuration.
+    path: {
+      win32: 'build/<%= pkg.name %>-win32-ia32',
+      win32_installer: 'build/win32-ia32-installer',
+    },
+    electron_version: '0.33.6',
+
     pkg: grunt.file.readJSON('package.json'),
     'create-windows-installer': {
       ia32: {
-        appDirectory: 'build/<%= pkg.name %>-win32-ia32',
-        outputDirectory: 'build/win32-ia32-installer',
-        authors: 'Thomas Lee',
+        appDirectory: '<%= path.win32 %>',
+        outputDirectory: '<%= path.win32_installer %>',
         remoteReleases: 'https://github.com/ThomasLee969/net.tsinghua'
       }
     },
     shell: {
       win32: {
         command: 'electron-packager . <%= pkg.name %> --platform=win32 ' +
-                 '--arch=ia32 --asar=true --out=build/ --version=0.33.6 ' +
-                 '--ignore=build/ --icon=resource/icon.ico ' +
-                 '--app-version=<%= pkg.version %>'
+                 '--arch=ia32 --out=build/ --version=<%= electron_version %> ' +
+                 '--asar=true --ignore=build/ --icon=resource/icon.ico ' +
+                 '--app-version=<%= pkg.version %> --overwrite=true'
       },
       darwin: {
         command: 'electron-packager . <%= pkg.name %> --platform=darwin ' +
-                 '--arch=x64 --out=build/ --version=0.33.6 ' +
+                 '--arch=x64 --out=build/ --version=<%= electron_version %> ' +
                  '--ignore=build/ --icon=resource/icon.icns ' +
-                 '--app-version=<%= pkg.version %>'
-      },
-      'clean-win32': {
-        command: 'rmdir /s /q build\\<%= pkg.name %>-win32-ia32',
-        options: {failOnError: false}
-      },
-      'clean-win32-installer': {
-        command: 'rmdir /s /q build\\win32-ia32-installer',
-        options: {failOnError: false}
-      },
-      'clean-darwin': {
-        command: 'rm -rf build/<%= pkg.name %>-darwin-x64/'
+                 '--app-version=<%= pkg.version %> --overwrite=true'
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-electron-installer');
 
-  grunt.registerTask('win32', ['shell:clean-win32', 'shell:win32',
-                               'shell:clean-win32-installer',
-                               'create-windows-installer']);
-  grunt.registerTask('osx', ['shell:clean-darwin', 'shell:darwin']);
-  grunt.registerTask('clean', ['shell:clean-darwin', 'shell:clean-win32',
-                               'shell:clean-win32-installer']);
+  grunt.registerTask('win32', ['shell:win32', 'create-windows-installer']);
+  grunt.registerTask('osx', ['shell:darwin']);
 };
