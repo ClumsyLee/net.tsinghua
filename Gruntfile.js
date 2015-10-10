d922aa2225bc08df0db6aa39bb0a947de0d1fbb7
Thomas Lee <lisihan969@gmail.com>
2015-10-10 10:09:35 +0800

    Clean Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,12 +8,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     'create-windows-installer': {
-      // x64: {
-      //   appDirectory: 'build/<%= pkg.name %>-win32-x64',
-      //   outputDirectory: 'build/',
-      //   authors: 'Thomas Lee',
-      //   remoteReleases: 'https://net-tsinghua.herokuapp.com/update/win32/' + version
-      // },
       ia32: {
         appDirectory: 'build/<%= pkg.name %>-win32-ia32',
         outputDirectory: 'build/win32-ia32-installer',
@@ -23,15 +17,22 @@ module.exports = function(grunt) {
     },
     shell: {
       win32: {
-        command: 'electron-packager . <%= pkg.name %> --platform=win32 --arch=ia32 --asar=true --out=build/ --version=0.33.6 --ignore=build/ --icon=resource/icon.ico --app-version=<%= pkg.version %>'
+        command: 'electron-packager . <%= pkg.name %> --platform=win32 '
+                 '--arch=ia32 --asar=true --out=build/ --version=0.33.6 '
+                 '--ignore=build/ --icon=resource/icon.ico '
+                 '--app-version=<%= pkg.version %>'
       },
       darwin: {
-        command: 'electron-packager . <%= pkg.name %> --platform=darwin --arch=x64 --out=build/ --version=0.33.6 --ignore=build/ --icon=resource/icon.icns --app-version=<%= pkg.version %>'
+        command: 'electron-packager . <%= pkg.name %> --platform=darwin '
+                 '--arch=x64 --out=build/ --version=0.33.6 '
+                 '--ignore=build/ --icon=resource/icon.icns '
+                 '--app-version=<%= pkg.version %>'
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-electron-installer');
 
-  grunt.registerTask('default', ['shell', 'create-windows-installer']);
+  grunt.registerTask('win32', ['shell:win32', 'create-windows-installer']);
+  grunt.registerTask('osx', ['shell:darwin']);
 };
