22478fcb1d0b5398d19f353883372b681d2e0cd3
Thomas Lee <lisihan969@gmail.com>
2015-10-18 18:21:10 +0800

    Add cert & setup icon for win installer

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,6 +29,8 @@ module.exports = function(grunt) {
       ia32: {
         appDirectory: '<%= path.win32 %>',
         outputDirectory: '<%= path.win32_installer %>',
+        certificateFile: 'resource/cert.cer',
+        setupIcon: 'resource/icon.ico',
         remoteReleases: 'https://github.com/ThomasLee969/net.tsinghua'
       }
     },
