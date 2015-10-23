0a43208a29f96cde13fd8e15f55bd75331003493
Thomas Lee <lisihan969@gmail.com>
2015-10-23 18:35:33 +0800

    Use custom keychain

---
+++ install_identity.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Create keychain"
+security create-keychain -p $CERTIFICATE_PASSWORD electron-app-build.keychain
+
+KEYCHAIN=~/Library/Keychains/electron-app-build.keychain
+
+echo "Adding distribution key"
+security import resource/cert.p12 -k $KEYCHAIN -P $KEY_PASSWORD -T /usr/bin/codesign
+
+echo "Unlock keychain"
+security unlock-keychain -p $CERTIFICATE_PASSWORD electron-app-build.keychain
+
+echo "Increase keychain unlock timeout"
+security set-keychain-settings -lut 7200 electron-app-build.keychain
+
+echo "Add keychain to keychain-list"
+security list-keychains -s electron-app-build.keychain
