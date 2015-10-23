cf26cd7cd5dc5ad2397075e444be7f3e14402498
Thomas Lee <lisihan969@gmail.com>
2015-10-23 13:58:30 +0800

    Use path.join instead of / in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
   require('load-grunt-tasks')(grunt);
   var mkdirp = require('mkdirp');
+  var join = require('path').join;
 
   var version = grunt.file.readJSON('package.json').version;
 
@@ -9,19 +10,19 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // Custom configuration.
     path: {
-      win32: 'build/<%= pkg.name %>-win32-ia32',
-      win32_installer: 'build/win32-ia32-installer',
-      darwin: 'build/<%= pkg.name %>-darwin-x64',
-      release: 'build/release/<%= pkg.version %>'
+      win32: join('build', '<%= pkg.name %>-win32-ia32'),
+      win32_installer: join('build', 'win32-ia32-installer'),
+      darwin: join('build', '<%= pkg.name %>-darwin-x64'),
+      release: join('build', 'release', '<%= pkg.version %>')
     },
     file: {
-      setup_exe: '<%= path.win32_installer %>/Setup.exe',
-      RELEASES: '<%= path.win32_installer %>/RELEASES',
-      nupkg_full: '<%= path.win32_installer %>/<%= pkg.name %>-<%= pkg.version %>-full.nupkg',
-      nupkg_delta: '<%= path.win32_installer %>/<%= pkg.name %>-<%= pkg.version %>-delta.nupkg',
-      win32_zip: '<%= path.release %>/<%= pkg.name %>-v<%= pkg.version %>-win32-ia32.zip',
-      darwin_zip: '<%= path.release %>/<%= pkg.name %>-v<%= pkg.version %>-darwin-x64.zip',
-      darwin_dmg: '<%= path.release %>/<%= pkg.name %>-v<%= pkg.version %>-darwin-x64.dmg'
+      setup_exe: join('<%= path.win32_installer %>', 'Setup.exe'),
+      RELEASES: join('<%= path.win32_installer %>', 'RELEASES'),
+      nupkg_full: join('<%= path.win32_installer %>', '<%= pkg.name %>-<%= pkg.version %>-full.nupkg'),
+      nupkg_delta: join('<%= path.win32_installer %>', '<%= pkg.name %>-<%= pkg.version %>-delta.nupkg'),
+      win32_zip: join('<%= path.release %>', '<%= pkg.name %>-v<%= pkg.version %>-win32-ia32.zip'),
+      darwin_zip: join('<%= path.release %>', '<%= pkg.name %>-v<%= pkg.version %>-darwin-x64.zip'),
+      darwin_dmg: join('<%= path.release %>', '<%= pkg.name %>-v<%= pkg.version %>-darwin-x64.dmg')
     },
     electron_version: '0.33.6',
 
@@ -61,11 +62,11 @@ module.exports = function(grunt) {
                  '--sign="Thomas Lee"'
       },
       zip_setup_exe: {
-        command: '7z a <%= file.win32_zip %> %APPVEYOR_BUILD_FOLDER%/<%= file.setup_exe %>'
+        command: '7z a <%= file.win32_zip %> ' + join('%APPVEYOR_BUILD_FOLDER%', '<%= file.setup_exe %>')
       },
       zip_app: {
         command: 'cd <%= path.darwin %> && zip -rq --symlinks ' +
-                 '../../<%= file.darwin_zip %> <%= pkg.name %>.app'
+                 join('..', '..', '<%= file.darwin_zip %>') + '<%= pkg.name %>.app'
       },
       move_win32_files: {
         command: 'move <%= file.RELEASES %> <%= file.nupkg_full %> ' +
